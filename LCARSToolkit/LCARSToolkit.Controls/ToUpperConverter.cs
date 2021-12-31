8b0e8394a9206201e273c1e4dc0b05ea2ecfa3f7
Steve Reckamp <sreckamp@gmail.com>
2021-12-31 11:41:49 -0600

    Add to upper case converter

---
+++ LCARSToolkit/LCARSToolkit.Controls/ToUpperConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LCARSToolkit.Controls
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    public class ToUpperConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is not string text) ? value : text.ToUpper();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
